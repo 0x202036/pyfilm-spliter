506ad8b631cfe983d80b772e49bd02ecaf366ce7
yimig <41770536+yimig@users.noreply.github.com>
2020-06-16 17:48:04 +0800

    添加生成错误报告功能

--- program.py
+++ program.py
@@ -1,6 +1,21 @@
-import analyser.analyser
-import analyser.caption_factory
-import re
+from analyser.analyser import *
+import traceback
+import time
 
-al = analyser.analyser.Analyser()
-print("done!")
+def write_doc(content_str: str):
+    content = 'date:'+time.asctime(time.localtime(time.time()))+'\n' \
+              '================================================\n'+ content_str
+    doc_file = open('runtime_doc.txt', 'w', encoding='utf-8')
+    doc_file.write(content)
+    doc_file.close()
+
+try:
+    an = Analyser()
+except Exception as e:
+    c = 'Exception Happened:\nstr(Exception):\t%s\nstr(e):\t%s\nrepr(e):\t%s\ntraceback.print_exc():\t%s\ntraceback.format_exc():\n%s' % \
+        (str(Exception), str(e), repr(e), traceback.print_exc(), traceback.format_exc())
+    write_doc(c)
+else:
+    c = 'Done!\nword_count:\t%s\nsentence_count:\t%s\nAll data have been uploaded in database.' % \
+        (str(len(an.word_list)), str(len(an.sentence_list)))
+    write_doc(c)
